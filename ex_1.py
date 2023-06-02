5ef8746724c6172d322d2a68bfdd36267eface2b
IvanGame <ivan5456007@gmail.com>
2023-06-02 22:15:37 +0700

    Seminar_3 is compleated

--- ex_1.py
+++ ex_1.py
@@ -1,17 +1,18 @@
-# Монетки
+# Сколько раз встерчается число
+import random
 
-n = int(input('Введите колличетсво монет'))
-
-orel = 0
-reshka = 0
+n = int(input('Введите размер массива'))
+m = int(input('Введите число для обозначения максимально возможного значения элемента массива'))
+x = int(input('Введите число для поиска'))
+count = 0
+s = []
 
 for i in range(n):
-    x = int(input())
-    if x == 1:
-        orel += 1
-    else:
-        reshka += 1
-if orel >= reshka:
-    print(f"Нужно перевернуть {reshka} монет")
-else:
-    print(f"Нужно перевернуть {orel} монет")
+    s.append(random.randint(1, m))
+print(s)
+
+for i in s:
+    if i == x:
+        count += 1
+print(
+    f'число {x} встречается в массиве {count} раз')
--- ex_2.py
+++ ex_2.py
@@ -1,11 +1,25 @@
-# Петя и Катя
+# Найти самый близкий элемент
 
-sum = int(input("Введите сумму чисел"))
-mult = int(input("Введите произведение чисел"))
+import random
 
-for i in range(sum):
-    for j in range(mult):
-        if i + j == sum and i * j == mult:
-            num_1 = i
-            num_2 = j
-print(f"Первое число - {num_1}, второе число - {num_2}")
+n = int(input('Введите размер массива\n'))
+m = int(input('Введите число для обозначения максимально возможного значения элемента массива\n'))
+x = int(input('Введите число для поиска ближайшего к нему\n'))
+y = m
+num = 0
+pos = 0
+s = []
+
+for i in range(n):
+    s.append(random.randint(1, m))
+print(s)
+
+for i in range(len(s) - 1):
+    if abs(x - s[i]) < y:
+        y = abs(x - s[i])
+        num = s[i]
+        pos = i
+        print(num, pos)
+
+print(
+    f'Самое близкое число к числу {x} является {num} находится на {pos + 1} позиции в массиве')
